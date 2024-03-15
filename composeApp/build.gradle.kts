90f904b9f0b81e5ccb3132933d0e4be1db2914ea
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-03-15 11:54:01 +0530

    Update java version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,7 +29,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
     }
@@ -88,8 +88,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
