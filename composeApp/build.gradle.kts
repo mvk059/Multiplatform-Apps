e6253494ab0453c56964708580a8ad0143fa8a39
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-03-20 12:26:57 +0530

    Add navigation android dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,6 +50,7 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.compose.navigation)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
