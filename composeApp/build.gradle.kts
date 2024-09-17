4011107cd0d1d28325eda5885254563515ca7b9d
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-09-17 14:17:48 +0530

    Remove iOS module

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -34,17 +34,6 @@ kotlin {
         }
     }
     
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-    
     sourceSets {
         
         androidMain.dependencies {
